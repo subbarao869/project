54e1e14d009e419d195233b47962bee8a60284c9
91701 <91701@192.168.0.3>
2019-06-26 22:09:08 +0530

    +ve,-ve/Zero

---
+++ java1/src/PNZ.java
@@ -0,0 +1,15 @@
+
+public class PNZ {
+	public static void main(String args[])
+	{
+		int n=-10; 
+		if(n>0)
+			System.out.print("Positive");
+		else if(n<0)
+			System.out.print("Negative");
+		else
+			System.out.print("Zero");
+		
+	}
+	
+}
